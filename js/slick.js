5be2b4b44aafeb5de296de6e5ee77cd43cad4365
Slava <110696118+sl7one@users.noreply.github.com>
2023-07-19 17:36:55 +0300

    finish main page

--- js/slick.js
+++ js/slick.js
@@ -1,3 +1,7 @@
+const leftArrow =
+  '<button class="icon-arrow-slider-left arrow-btn-slider" type="button"></button>';
+const rightArrow =
+  '<button class="icon-arrow-slider-right arrow-btn-slider" type="button"></button>';
 $(document).ready(function () {
   $(".slider").slick({
     infinite: true,
@@ -5,16 +9,45 @@ $(document).ready(function () {
     appendArrows: $(".slider__arrows"),
     autoplaySpeed: 5000,
     zIndex: -1,
-    nextArrow: '<button class="icon-arrow right" type="button"></button>',
-    prevArrow: '<button class="icon-arrow left" type="button"></button>',
+    nextArrow:
+      '<button class="icon-arrow-slider-right arrow-btn-slider" type="button"></button>',
+    prevArrow:
+      '<button class="icon-arrow-slider-left arrow-btn-slider" type="button"></button>',
+  });
+
+  $(".news__slider").slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // appendArrows: $(".slick-dots"),
+    // autoplaySpeed: 5000,
+    zIndex: -1,
+    // nextArrow: rightArrow,
+    // prevArrow: leftArrow,
   });
 });
 
 const setNumbers = (_, slick) => {
   const { currentSlide, slideCount } = slick;
-  $(".current-slide").text((currentSlide + 1).toString().padStart(2,'0')+"/");
-  $(".total-slides").text(slideCount.toString().padStart(2,'0'));
+  $(".current-slide").text(
+    (currentSlide + 1).toString().padStart(2, "0") + "/"
+  );
+  $(".total-slides").text(slideCount.toString().padStart(2, "0"));
 };
 
 $(".slider").on("init", setNumbers);
 $(".slider").on("beforeChange", setNumbers);
+
+$(".news__slider").on("init", (_, slick) => {
+  $(".slick-dots").wrapAll("<div class='custom-wrapper' />");
+  $(".custom-wrapper").prepend(leftArrow).append(rightArrow);
+
+  const triggerNextSlide = () => $(".news__slider").slick("slickNext");
+  const triggerPrevSlide = () => $(".news__slider").slick("slickPrev");
+
+  $(".custom-wrapper .icon-arrow-slider-left").on("click", triggerPrevSlide);
+  $(".custom-wrapper .icon-arrow-slider-right").on("click", triggerNextSlide);
+});
