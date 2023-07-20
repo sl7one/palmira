f4866b607c1e6150217c5b6d6b1ca6e609c95d3d
Slava <110696118+sl7one@users.noreply.github.com>
2023-07-20 23:34:14 +0300

    upd mobile menu

--- js/burger-btn.js
+++ js/burger-btn.js
@@ -1,12 +1,37 @@
-$(".menu").click(function () {
-  $(this).toggleClass("open");
+$(".menu").on("click", () => {
+  $(".menu").toggleClass("open");
 
-  if ($(this).hasClass("open")) {
+  if ($(".menu").hasClass("open")) {
     $(".mobile-menu__modal").addClass("visible");
     $("body").addClass("overflow");
   } else {
     $(".mobile-menu__modal").removeClass("visible");
     $("body").removeClass("overflow");
-
+    $(".mobile-menu__nav-menu > li > ul").removeClass("visible");
+    $(".mobile-menu__nav-menu > li").css("position", "relative");
+    $(".mobile-menu__back-btn").removeClass("visible");
   }
+});
+
+$(".mobile-menu__nav-menu > li > a").on("click", e => {
+  const { target } = e;
+  const children = target.parentElement.parentElement.children;
+  const modalTrigger =
+    children[0].children[0] === target ||
+    children[2].children[0] === target ||
+    children[5].children[0] === target;
+
+  if (modalTrigger) {
+    e.preventDefault();
+
+    $(".mobile-menu__nav-menu > li > ul").addClass("visible");
+    $(".mobile-menu__nav-menu > li ").css("position", "static");
+    $(".mobile-menu__back-btn").addClass("visible");
+  }
+});
+
+$(".mobile-menu__back-btn").on("click", () => {
+  $(".mobile-menu__back-btn").removeClass("visible");
+  $(".mobile-menu__nav-menu > li > ul").removeClass("visible");
+  $(".mobile-menu__nav-menu > li").css("position", "relative");
 });
